3a659fd5e15621ce76f59290243c589e4789cf35
mlikasam <samir.mlika@askui.com>
2022-05-17 12:43:07 +0200

    refactor: try to fix piepline (#AS-923)

--- examples/askui-nodejs-base-example/test/my-first-askui-test-suite.test.ts
+++ examples/askui-nodejs-base-example/test/my-first-askui-test-suite.test.ts
@@ -2,19 +2,10 @@ import { AskuiClient } from '@vqa4gui/askui';
 
 describe('jest with askui', () => {
   // Client is necessary to use the askui API
-  let askuiClient: AskuiClient;
-  const controluiServerUrl = process.env.CI_JOB_ID ? 'askui-runner' : 'localhost';
-
-  beforeAll(async () => {
-    askuiClient = new AskuiClient({
-      controlServerUrl: `http://${controluiServerUrl}:6769`,
-    });
-
-    await askuiClient.connect();
-  });
+  let aui: AskuiClient;
 
   it('should click on text', async () => {
-    await askuiClient
+    await aui
       .click()
       .text()
       .exec();
